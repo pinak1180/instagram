ccc7bdec4a007e61011a441c7dbaf6cdd53d1195
Pinak <pinak@complitech.net>
2013-11-20 15:33:11 +0530

    config vars

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,8 +1,8 @@
 require "instagram"
 
 Instagram.configure do |config|
-  config.client_id = CLIENT_ID
-  config.client_secret = CLIENT_SECRET
+  config.client_id = ENV['CLIENT_ID']
+  config.client_secret = ENV['CLIENT_SECRET']
 end
 
 CALLBACK_URL = "http://guarded-fjord-7351.herokuapp.com/session/callback"
